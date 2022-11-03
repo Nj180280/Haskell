de49a6e39a4a499521626ce92ea2dffacda56318
Nj180280 <97845995+Nj180280@users.noreply.github.com>
2022-11-03 23:25:56 +0530

    Add files via upload

---
+++ reapply.hs
@@ -0,0 +1,12 @@
+reapply f x=f(f x)
+square x=x*x
+double x=x*2
+half x=x/2
+
+main=do
+     putStrLn"Enter number :"
+     ain<-getLine
+     let a=(read ain)
+     print(reapply square a)
+     print(reapply double a)
+     print(reapply half a)
